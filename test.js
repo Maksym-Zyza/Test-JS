dd325a39cfd2a630f95c021ca9582cd9f245e0b5
Maksym-Zyza <mmaksymziza@gmail.com>
2020-08-13 11:55:51 +0300

    Условия - Функции

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-alert("Hello word!");
+// alert("Hello word!");
 
 //ПЕРЕМЕННАЯ и ее обьявление
 // ES6 - let, const.    ES5 - var.
@@ -11,7 +11,7 @@ variable = 5; //обьявление переменной (литерал)
 
 // let a = 5;
 // let b = 10;
-// console.log(a + b);
+// console.log(a + b); - Оператор.
 
 // let a = (b = 20);
 // console.log(a * b);
@@ -69,7 +69,7 @@ console.log(typeof y);
 // Обьект содержыт - Свойства - Функции
 let car = {
   color: `green`,
-  maxSped: 120,
+  maxSped: 240,
   audio: {
     brand: "sony",
     speakers: 12,
@@ -87,3 +87,88 @@ console.log(car.color);
 console.log(car.audio.brand);
 
 //УСЛОВИЯ
+// if (Условие - true) {
+//     Оператор - console.log(car);
+// }
+
+// if -если     == - Истина    != - Ложь      && - и       || - или     else - Иначче       else if - Иначче если
+// !- оператор отрицания    (!true - не истина)
+
+if (car.color == "red") {
+  console.log("Автомобиль красный");
+} else if ((car.color = "grenn")) {
+  console.log("Автомобиль зеленый");
+} else {
+  console.log("Цвет не определен");
+}
+
+// Сокращенный условний тернарный оператор if
+car.color == "grenn" ? console.log("Зеленый") : console.log("Красный");
+
+let color = car.color == "red" ? "Красный" : "Зеленый";
+console.log(color);
+
+//ФУНКЦИЯ
+car.color = "green";
+console.log(car.color);
+
+function colorReset() {
+  car.color = "orange";
+  console.log(car.color);
+}
+
+// Вызов функции (можно ввызывать в много раз в любом менсте, принято к конце страницы)
+colorReset();
+// Функция должна выполнять одно действиве!
+
+// Зададим цвет второго автомобиля
+let car2 = {
+  color: "tomato",
+};
+
+// В скобках функции указывается параметр
+function colorChenge(theCar) {
+  theCar.color = "white";
+}
+
+// Вызываем ф-ю для первого и второго аитомобиля
+colorChenge(car);
+colorChenge(car2);
+
+console.log("Car:", car.color);
+console.log("Car2:" + car2.color);
+// При вызове "," - пробел, "+" - без пробела
+
+//  Вызов ф-и через имя переменной - указываеться без "()"
+let speed = spedChange;
+//  Обращение со "()"
+speed();
+
+function spedChange() {
+  console.log(car.maxSped);
+}
+
+// Если в ф-и есть входной параметр watch(message), для него нужно задать значение
+// function watch(message = "Hello") {
+//   console.log(message);
+// }
+// watch();
+
+// Функция возвращения - return
+function watch(message) {
+  return message + " - Vasiliy";
+  // console.log(test); - после "return" нельзя делать записи - код становиться недоступен
+}
+let message = watch("Hello");
+console.log(message);
+
+// Анонимные функции
+let anonym = function () {
+  console.log("we are anonymus");
+};
+anonym();
+
+// Краткая запись анонисной ф-и
+(function () {
+  console.log("we are anonymus");
+})();
